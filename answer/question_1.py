70dd993b8105406823685ca19abb96db9462612f
mostafajaman.bu.gm <mostafajaman.bu.gm@gmail.com>
2024-10-07 09:10:10 +0000

    first commit

---
+++ answer/question_1.py
@@ -0,0 +1,17 @@
+import math
+
+def primeChecker(number):
+    if number > 1:
+        for i in range(2, int(math.sqrt(number)) + 1):
+            if number % i == 0:
+                return False
+                break
+        return True
+    else:
+        return False
+
+number = int(input("Enter a number: "))
+if primeChecker(number):
+    print("Prime")
+else:
+    print("Not Prime")
